92422b5ec121d4c3245af3ad1a838e774bbdbcca
TimCauss <t.caussignac@gmail.com>
2023-06-07 14:33:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const passInputElem = document.getElementById("password");
+const checkInputElem = document.getElementById("check-password");
+const message = document.getElementById("message");
+
+function Checking() {
+  passInputElem.value != checkInputElem.value
+    ? (message.innerHTML = "Passwords don't match!")
+    : (message.innerHTML = "");
+}
+
+passInputElem.addEventListener("keyup", Checking);
+checkInputElem.addEventListener("keyup", Checking);
